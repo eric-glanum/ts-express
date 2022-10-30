7fa0c4f8aafda763e6935e3b80e009c8b7e9a9b9
eric-glanum <eric@glanum.com>
2022-10-30 13:42:39 +0100

    feat/db connection: add mysql connection to db and automatically create db if it doesn't exists

---
+++ src/db/db.config.ts
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+export default {
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 const express = require("express");
 import { Response, Request } from "express";
+import mysql from "mysql";
+import dbConfig from "../src/db/db.config";
 
 const app = express();
+
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "server started" });
@@ -9,4 +17,16 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(3000, () => {
   console.log("connected to server");
+});
+
+connection.connect((err) => {
+  err
+    ? console.log("Error : ", err)
+    : console.log("Succesfully connected to DB");
+  connection.query(
+    `CREATE DATABASE IF NOT EXISTS ${dbConfig.DB}`,
+    (err, res) => {
+      err ? console.log("Error : ", err) : res;
+    }
+  );
 });
